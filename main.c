e9eff5cb129a2f2693323d87655fb41f3e8f661b
Olgierd <opilarczyk@gmail.com>
2015-11-02 09:50:12 +0100

    new main - udp sending

--- main.c
+++ main.c
@@ -2,20 +2,35 @@
 #include <pulse/simple.h>
 #include <pulse/error.h>
 #include <math.h>
+#include<string.h> //memset
+#include<stdlib.h> //exit(0);
+#include<arpa/inet.h>
+#include<sys/socket.h>
+
+
+#define SERVER "127.0.0.1"
+#define BUFLEN 512  //Max length of buffer
+#define PORT 8888   //The port on which to send data
+ 
+
+void die(char *s)
+{
+    perror(s);
+    exit(1);
+}
 
 
 int main(int argc, char *argv){
+
+
+// init pulseaudio
 	pa_simple *s;
 
-	static const pa_sample_spec ss = {
-		.format = PA_SAMPLE_U8,
-		.rate = 8000,
-		.channels = 1
-	};
+	static const pa_sample_spec ss = { .format = PA_SAMPLE_U8, .rate = 8000, .channels = 1 };
 
 	s= pa_simple_new(NULL,			//default server
 		"Voipi_play",		//app name
-		PA_STREAM_PLAYBACK,
+		PA_STREAM_RECORD,
 		NULL,			//default device
 		"network sound playback", //stream description
 		&ss,			//sample format
@@ -24,26 +39,40 @@ int main(int argc, char *argv){
 		NULL			//Ignore error code
 	);
 
-	uint8_t buf[100];
-	int x;
-	int error;
-	srand(time(0));
 
-	double sinpos=0;
-	char beacon[] = { 1,0,1,0,1,0,0,0,1,0,1,1,1,0,1,0,0,0,1,0,1,0,1,0,1,1,1,0,1,1,1,0,0,0,1,0,1,0,1,0,1,1,1,0,0,0,1,0,1,0,1,0,1,0,0,0,1,1,1,0,1,0,1,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+
+// init UDP socket 
+
+	struct sockaddr_in si_other;
+	int sck, i, slen=sizeof(si_other);
+	unsigned char buf[BUFLEN];
+
+
+	if ( (sck=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+	{
+		die("socket");
+	}
+
+	memset((char *) &si_other, 0, sizeof(si_other));
+	si_other.sin_family = AF_INET;
+	si_other.sin_port = htons(PORT);
+
+	if (inet_aton(SERVER , &si_other.sin_addr) == 0) 
+	{
+		fprintf(stderr, "inet_aton() failed\n");
+		exit(1);
+	}
+
+// end of socket init
+
+	int error;	
 
 	while(1) {
-		for(x=0; x<100; x++) { 
-			buf[x] = (uint8_t)(rand()%135);
-			if(beacon[(int)sinpos/130]) {
-				buf[x] += 50*sin(sinpos); 
-			}
-			sinpos += 0.25;
-			if(sinpos/130 >= sizeof(beacon)/sizeof(uint8_t)) sinpos =0; 
-		}     // make some noise !!
 	
 			
-		pa_simple_write(s, buf, 100, &error);
+		pa_simple_read(s, buf, BUFLEN, NULL);
+		sendto(sck, buf, BUFLEN, 0 , (struct sockaddr *) &si_other, slen);
+
 	}
 
 
